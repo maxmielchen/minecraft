00f14ce07ed24420d09dd8c498553f9d8f591b74
Max Mielchen <94404446+maxmielchen@users.noreply.github.com>
2023-05-06 15:23:41 +0200

    Update build.py

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ def main():
         runtime_version_list = data["runtime"]
         runtimes = []
         print("Load runtimes...")
-        for runtime_version_key, runtime_version_value in tqdm(runtime_version_list.items()):
+        for runtime_version_key, runtime_version_value in runtime_version_list.items():
             version = runtime_version_key
             runtimes_list = runtime_version_value
             for runtime in runtimes_list:
@@ -25,15 +25,17 @@ def main():
             docker_info = docker_client.info()
             docker_ping = docker_client.ping()
             print(" --Docker-- ")
+            print(f"Info: {docker_info}")
             print(f"Version: {docker_version}")
+            print(f"Ping: {docker_ping}")
+            print(docker_ping)
             print(" --Docker-- ")
         except Exception:
             print("\033[91mCOULD NOT CONNECT TO DOCKER!")
             exit(1)
 
-
         server_type_list = data["server"]
-        for server_type_key, server_type_value in tqdm(server_type_list.items()):
+        for server_type_key, server_type_value in server_type_list.items():
             server_type_name = server_type_key
             server_list = server_type_value
             for server in server_list:
@@ -47,20 +49,26 @@ def main():
                     runtime_name = runtime_properties["name"]
                     image = runtime_properties["image"]
 
+                    print(f"Try to build -> minecraft:{server_type_name}-{server_version}-{runtime_name}-{runtime_version}")
+
                     builded_image, logs = docker_client.images.build(
                         dockerfile="./Dockerfile",
                         path=".",
                         tag=f"minecraft:{server_type_name}-{server_version}-{runtime_name}-{runtime_version}",
                         nocache=True,
                         buildargs={
-                            'http_source': server_source,
+                            'source': server_source,
                             'artifact': server_artifact,
                             'image': image
                         }
                     )
 
+                    print(f"Successfully build -> minecraft:{server_type_name}-{server_version}-{runtime_name}-{runtime_version}")
+
                     builded_image.tag(registry="ghcr.io", repository="maxmielchen/minecraft", tag=f"{server_type_name}-{server_version}-{runtime_name}-{runtime_version}")
                     docker_client.images.push(repository='ghcr.io/maxmielchen/minecraft', tag=f"{server_type_name}-{server_version}-{runtime_name}-{runtime_version}")
+
+                    print(f"Successfully pushed -> minecraft:{server_type_name}-{server_version}-{runtime_name}-{runtime_version}")
 
 
 if __name__ == '__main__':
