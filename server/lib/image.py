9ef2f8c55da9f2c83a2e6852a9561fca7d00a5a8
maxmielchen <max@mielchen.email>
2023-05-13 10:20:59 +0200

    add console output

--- server/lib/image.py
+++ server/lib/image.py
@@ -12,8 +12,9 @@ class Image:
         self.server: Server = server
         self.runtime: Server = runtime
 
-    def build(self, docker_client: DockerClient, instant: bool = False):
+    def build(self, docker_client: DockerClient, infos: bool = True, instant: bool = False):
         "Builds the minecraft image and can push it if necessary"
+
         builded_image, _ = docker_client.images.build(
             dockerfile="./Dockerfile",
             path=".",
@@ -26,13 +27,19 @@ class Image:
                 'image': self.runtime.image
             }
         )
+
         builded_image.tag(repository=f"{self.base_environments.get_registry()}/{self.base_environments.get_repository()}", tag=f"{self.server.server}-{self.server.version}-{self.runtime.name}-{self.runtime.java_version}-latest")
         builded_image.tag(repository=f"{self.base_environments.get_registry()}/{self.base_environments.get_repository()}", tag=f"{self.server.server}-{self.server.version}-{self.runtime.name}-{self.runtime.java_version}-{self.base_environments.get_release_tag()}")
+        
+        if infos:
+            print(f"Successfully deploy: \n {self.server} \n {self.runtime} \n")
 
         if instant:
             docker_client.images.push(repository=f"{self.base_environments.get_registry()}/{self.base_environments.get_repository()}")
 
-    def push(docker_client: DockerClient):
+    def push(docker_client: DockerClient, infos: bool = True):
         "Ensures that all images are pushed into a repository"
         base_environments = BaseEnvironments()
         docker_client.images.push(repository=f"{base_environments.get_registry()}/{base_environments.get_repository()}")
+        if infos:
+            print(f"Successfully pushed to {base_environments.get_registry()}/{base_environments.get_repository()}")
