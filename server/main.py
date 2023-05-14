20f58f52a59440429451323ba7a1d8153ca617a7
maxmielchen <max@mielchen.email>
2023-05-14 13:41:21 +0200

    semaphore join

--- server/main.py
+++ server/main.py
@@ -7,7 +7,13 @@ from lib.image import Image
 from lib.runtimepool import RuntimePool
 from lib.serverpool import ServerPool
 
-from multiprocessing import Pool
+from multiprocessing import Pool, Process, Semaphore
+from multiprocessing.synchronize import Semaphore as SemaphoreClass
+
+def semaphore_join(pool: Pool, semaphore: SemaphoreClass):
+    "Terminates the given pool and frees the semaphore"
+    pool.join()
+    semaphore.release()
 
 if __name__ == '__main__':
     with open("matrix.json") as matrix:
@@ -32,7 +38,12 @@ if __name__ == '__main__':
                 results.append(result)
 
             pool.close()
-            pool.join(timeout=5.8 * 60 * 60)
+
+            join_semaphore = Semaphore()
+            join_process = Process(target=semaphore_join, args=(pool, join_semaphore))
+            join_process.start()
+
+            join_semaphore.acquire(True, 5.8 * 60 * 60)
 
             if not all(result.ready() for result in results):
                 print("Couldn't finish building!")              # Sends a message if not all images could be built
